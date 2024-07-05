84190a8bd9af82caa0c7035e685c3806d6c565f9
YonasValentin <83760534+YonasValentin@users.noreply.github.com>
2024-07-05 17:21:52 +0200

    feat: Added copy error + code

--- src/extension.ts
+++ src/extension.ts
@@ -33,8 +33,13 @@ export function activate(context: vscode.ExtensionContext) {
                     JSON.stringify({ message })
                   )}`
                 );
+                const copyFullCommandUri = vscode.Uri.parse(
+                  `command:errorclipper.copyErrorAndCode?${encodeURIComponent(
+                    JSON.stringify({ message })
+                  )}`
+                );
                 const markdownString = new vscode.MarkdownString(
-                  `[Copy error to clipboard](${copyCommandUri})`
+                  `[Copy error to clipboard](${copyCommandUri})\n\n[Copy error and code](${copyFullCommandUri})`
                 );
                 markdownString.isTrusted = true;
 
@@ -59,6 +64,24 @@ export function activate(context: vscode.ExtensionContext) {
         );
       } else {
         vscode.window.showWarningMessage('No error message to copy.');
+      }
+    })
+  );
+
+  // Register the command to copy the error message and the full code
+  context.subscriptions.push(
+    vscode.commands.registerCommand('errorclipper.copyErrorAndCode', (args) => {
+      const editor = vscode.window.activeTextEditor;
+      if (args && args.message && editor) {
+        const { message } = args;
+        const fullText = editor.document.getText();
+        const combinedText = `Error: ${message}\n\nCode:\n${fullText}`;
+        vscode.env.clipboard.writeText(combinedText);
+        vscode.window.showInformationMessage(
+          'Error message and code copied to clipboard'
+        );
+      } else {
+        vscode.window.showWarningMessage('No error message or code to copy.');
       }
     })
   );
